500ab399994e42c13f952093e2f548e9a0ed7f27
seong2950 <seong2950g@gmail.com>
2025-06-06 06:07:15 +0900

    re uri

--- src/components/KakaoLogin.tsx
+++ src/components/KakaoLogin.tsx
@@ -1,7 +1,7 @@
 //const CLIENT_ID = "5286ca90bb92ab86336d4edd295f85cf";
 const CLIENT_ID = "388a5f94f1884d66b7654021db9f5c13";
-//const REDIRECT_URI = "https://2025-1-capstone.vercel.app/oauth/kakao";
-const REDIRECT_URI = "http://localhost:5173/oauth/kakao";
+const REDIRECT_URI = "https://2025-1-capstone.vercel.app/oauth/kakao";
+//const REDIRECT_URI = "http://localhost:5173/oauth/kakao";
 
 export const getKakaoLoginUrl = () => {
   return `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
