1c63d89e03e43f39e600411aa2b5782d00353c84
seong2950 <seong2950g@gmail.com>
2025-06-05 20:47:17 +0900

    fix redirect uri

--- src/components/KakaoLogin.tsx
+++ src/components/KakaoLogin.tsx
@@ -1,5 +1,5 @@
 const CLIENT_ID = "5286ca90bb92ab86336d4edd295f85cf";
-const REDIRECT_URI = "https://jeju-folktale.r-e.kr/auth/kakao";
+const REDIRECT_URI = "https://2025-1-capstone.vercel.app/auth/kakao";
 
 export const getKakaoLoginUrl = () => {
   return `https://kauth.kakao.com/oauth/authorize?client_id=${CLIENT_ID}&redirect_uri=${REDIRECT_URI}&response_type=code`;
